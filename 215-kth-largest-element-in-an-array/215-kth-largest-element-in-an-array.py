dd32c93fcf1348140e31561255b87ddac814eaf3
baranshad <36979586+baranshad@users.noreply.github.com>
2022-04-14 20:49:22 -0400

    Time: 92 ms (57.21%), Space: 14.7 MB (97.11%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap = []
+        for i in range(k):
+            minHeap.append(nums[i])
+            
+        for i in range(k, len(nums)):
+            minHeap.sort()
+
+            if (minHeap[0] > nums[i]):
+                continue
+        
+            else:
+                minHeap.pop(0)
+                minHeap.append(nums[i])
+        #print(sorted(minHeap))        
+        return sorted(minHeap)[0]
+
+                
+ 
+        
