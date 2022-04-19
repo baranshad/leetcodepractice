8500a42d8c413966db1b41617946da993da9d57e
baranshad <36979586+baranshad@users.noreply.github.com>
2022-04-18 21:01:31 -0400

    Time: 1528 ms (64.88%), Space: 35 MB (25.95%) - LeetHub

---
+++ 1852-distinct-numbers-in-each-subarray/1852-distinct-numbers-in-each-subarray.py
@@ -0,0 +1,23 @@
+class Solution:
+    def distinctNumbers(self, nums: List[int], k: int) -> List[int]:
+        if k > len(nums):
+            return 0 
+        
+        ans  =  []
+        word_freq = {}
+        i = 0
+        j = 0 
+        while j < len(nums):
+            if nums[j] not in word_freq:
+                word_freq[nums[j]] = 0
+            word_freq[nums[j]] += 1    
+            
+            if j-i+1 == k:
+                ans.append(len(word_freq))
+                word_freq[nums[i]] -= 1 
+                if word_freq[nums[i]] == 0:
+                    del word_freq[nums[i]]
+                i += 1 
+            if j-i+1 < k:
+                j+= 1 
+        return ans 
