541e3e9aac41476e53061a934855f7bac77c3cd9
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-09 21:23:24 -0400

    Time: 81 ms (9.09%), Space: 14.2 MB (22.79%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,16 +1,18 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        def helper(temp, used):
-            if len(temp)== len(nums):
-                res.append(temp[:])
+        def helper(stack, used):
+            if len(stack) == len(nums):
+                res.append(stack[:])
+                
             for i in range(len(nums)):
                 if used[i] == True:
                     continue 
                 used[i] = True 
-                temp.append(nums[i])
-                helper(temp, used)
-                temp.pop()
-                used[i] = False
+                stack.append(nums[i])
+                helper(stack, used)
+                stack.pop()
+                used[i] = False 
+                
         helper([], [False]*len(nums))
         return res 
