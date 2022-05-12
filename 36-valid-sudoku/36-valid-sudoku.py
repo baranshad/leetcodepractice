1d4d4a1bc7e7f8a40298401ceb5f8af164d976d9
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-12 10:13:59 -0400

    Time: 153 ms (29.99%), Space: 13.8 MB (83.63%) - LeetHub

---
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -0,0 +1,27 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        N = 9 
+        r = [set() for _ in range(N)]
+        c = [set() for _ in range(N)]
+        b = [set() for _ in range(N)]
+        
+        for i in range(N):
+            for j in range(N):
+                val = board[i][j]
+                if val == '.':
+                    continue 
+                
+                if val in r[i]:
+                    return False 
+                r[i].add(val)
+                
+                if val in c[j]:
+                    return False 
+                c[j].add(val)
+                
+                idx = (i//3)*3 + j //3 
+                if val in b[idx]:
+                    return False 
+                b[idx].add(val)
+                
+        return True 
