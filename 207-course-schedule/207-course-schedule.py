1297ee1baacc4a837177de078ee7ab6227fade47
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-14 11:43:24 -0400

    Time: 867 ms (5.01%), Space: 15.5 MB (88.11%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,23 +1,20 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # create adjacency list
-        d = defaultdict(list)
+        d = defaultdict(list) # create adjacency list
         for i, j in prerequisites:
             d[i].append(j)
-            #graph[b].append(a)  # no, because graph is directional!
                         
         for n in range(0,numCourses):
             Q = deque(d[n])
-            #res = []
             visited = set()
             while Q:
                 #m = Q.popleft()  # --> BFS
                 m = Q.pop()     # --> DFS
                 if m==n:
                     return False # we detected a loop
-                visited.add(m)  # this is just for optimization, not strictly needed
-                for neighbor in d[m]:
-                    if neighbor not in visited:
-                        Q.append(neighbor)
+                visited.add(m)  
+                for nb in d[m]:
+                    if nb not in visited:
+                        Q.append(nb)
         
         return True
