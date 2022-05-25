786f6b829ae232dbf388bd31d9cc83ad292a7268
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-24 20:50:49 -0400

    Time: 305 ms (60.64%), Space: 16.6 MB (31.96%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -1,16 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         ans = 0 
-        partialsum =  {}
-        partialsum[0] = 1 
+        partialsum =  defaultdict(int)
+        #partialsum[0] = 1 
         pre = 0
         for i in nums:
             pre = pre + i 
-            
+            if pre == k:
+                ans +=1 
             if pre - k in partialsum:
                 ans += partialsum[pre-k] 
             
-            partialsum[pre] = partialsum.get(pre,0) + 1 
+            partialsum[pre] += 1 
         return ans 
  
 
