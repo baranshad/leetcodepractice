3f486d0ab99e0636a620116102af9c9e9f4691b6
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-31 22:26:31 -0400

    Time: 158 ms (78.71%), Space: 18.1 MB (84.18%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -4,7 +4,8 @@ class Solution:
         i = 0
         while i < len(s) -1:
             if s[i][1] >= s[i+1][0]:
-                s[i][1] = max(s[i][1], s[i+1][1])
+                #s[i][1] = max(s[i][1], s[i+1][1])
+                s[i] = [s[i][0], max(s[i][1], s[i+1][1])]
                 s.pop(i+1)
             else:
                 i += 1
