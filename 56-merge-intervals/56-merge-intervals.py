6a1c3faeb4f85ce523e10ea0864c4d0bfc2e0620
baranshad <36979586+baranshad@users.noreply.github.com>
2022-05-31 22:28:19 -0400

    Time: 295 ms (8.94%), Space: 18.1 MB (84.18%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -3,12 +3,11 @@ class Solution:
         s = sorted(intervals)
         i = 0
         while i < len(s) -1:
-            if s[i][1] >= s[i+1][0]:
-                #s[i][1] = max(s[i][1], s[i+1][1])
+            if s[i][1] < s[i+1][0]:
+                i +=1 
+            else:
                 s[i] = [s[i][0], max(s[i][1], s[i+1][1])]
                 s.pop(i+1)
-            else:
-                i += 1
         return s
 
 
