0afadeb7086d8744d5b408b1cadfbb22ab96e542
baranshad <36979586+baranshad@users.noreply.github.com>
2022-06-02 19:28:51 -0400

    Time: 148 ms (46.10%), Space: 17.2 MB (80.58%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,9 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if strs == [""]:
-            return [strs]
-        
-        d = defaultdict(list)
-        for s in strs:
-            d[tuple(sorted(s))].append(s)  
-        return d.values()
+        def helper(s1):
+            s1_list = sorted(list(s1)) 
+            return ''.join(s1_list)
+
+        d = {}
+        for i in strs:
+            
+            if helper(i) in d:
+                d[''.join(sorted(list(i)))].append(i)
+            else:
+                d[''.join(sorted(list(i)))] = [i]
+
+        ans = [i for i in d.values()]
+        return ans 
+            
+                
