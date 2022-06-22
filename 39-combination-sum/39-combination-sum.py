ade14e02abdda4a6c98cf591ad7bfd6c4791a03e
baranshad <36979586+baranshad@users.noreply.github.com>
2022-06-22 19:46:42 -0400

    Time: 151 ms (32.62%), Space: 14.2 MB (10.59%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,21 +1,25 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        if not candidates and min(candidates) > target:
-            return []
+        res = []
         
-        ans = []
-        def helper(path, idx):
-            if sum(path) == target:
-                ans.append(path[:])
-                
-            elif sum(path) < target:
-                for i in range(idx, len(candidates)):
-                    path.append(candidates[i])
-                    helper(path, i)
-                    path.pop()
+        def helper(idx, temp):
+            if sum(temp) == target:
+                res.append(temp[:])
+            
+            elif sum(temp) < target:
+                for i in range(idx,len(candidates)):
+                    temp.append(candidates[i])
+                    helper(i ,temp)
+                    temp.pop()
                     
-        helper([], 0)
-        return ans 
+        helper(0, [])
+        return res 
+                    
+                    
+    
+    
+     
+         
     
 
     
