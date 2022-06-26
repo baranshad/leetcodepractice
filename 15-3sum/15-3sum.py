52652000ab71654f1aa6da8e7efd67580006c1e0
baranshad <36979586+baranshad@users.noreply.github.com>
2022-06-26 17:51:24 -0400

    Time: 1034 ms (67.10%), Space: 18.7 MB (11.92%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,10 +1,11 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums = sorted(nums)
+        # sort it first, use the same trick as backtrack questions
+        nums = sorted(nums) 
         res = [] 
-        for i in range(len(nums)):
-            if i == 0 or nums[i] != nums[i-1]:
-                #cur = nums[i]
+        for i in range(len(nums)):# fix most left first, then adjust postions of l,r
+            if i == 0 or nums[i] != nums[i-1]:# avoid repeat
+                ## don't know why here [-2,0,0,2,2] doesn't work
                 l = i+1 
                 r = len(nums)-1 
                 while l < r:
@@ -13,8 +14,8 @@ class Solution:
                         res.append([nums[i], nums[l], nums[r]])
                         l += 1 
                         r -= 1 
-                        continue
-                    if three < 0:
+                        #continue
+                    elif three < 0:
                         l += 1 
                     else:
                         r -= 1 
