0d925770720233a2c7635d2f63f9b9b8fa2a4a84
baranshad <36979586+baranshad@users.noreply.github.com>
2022-06-26 19:10:14 -0400

    Time: 43 ms (92.55%), Space: 13.8 MB (74.49%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,24 +1,34 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
-        pt = 0 
-        for i in range(len(nums)-1, 0, -1):
+        ## find the first updown pattern
+        ## from the right to left, cur > left and cur > right 
+        ## if cur go until 0, then just return sorted(reverseorder)
+        n = len(nums)-1
+        if n == 0: 
+            return nums
+        if nums[-1] > nums[-2]:
+            nums[-1],nums[-2] = nums[-2],nums[-1]
+            return 
+        p= 0
+        for i in range(n, 0, -1):
             if nums[i] > nums[i-1]:
-                pt = i 
+                p = i
                 break
-        
-        if pt == 0:
+        print(p)
+        if p == 0:
             nums.sort()
             return 
-        j = len(nums) -1
-        while nums[pt-1] >= nums[j]:
-            j -=1 
+        elif p > 0:
+            j = len(nums)-1
+            while nums[p-1] >= nums[j]:
+                j -= 1 
+            nums[p-1], nums[j] = nums[j], nums[p-1]
+            print(nums[p:])
+            nums[p:] = sorted(nums[p:])
+        
             
-        nums[pt-1], nums[j] = nums[j], nums[pt-1]
-        nums[pt:] = reversed(nums[pt:])
-            
+        
+        
         
     
 
