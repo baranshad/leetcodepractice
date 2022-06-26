3fbbecb97b2b31d2a75716b4965a763d2266406c
baranshad <36979586+baranshad@users.noreply.github.com>
2022-06-26 19:11:34 -0400

    Time: 60 ms (56.51%), Space: 13.9 MB (74.49%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -8,22 +8,20 @@ class Solution:
             return nums
         if nums[-1] > nums[-2]:
             nums[-1],nums[-2] = nums[-2],nums[-1]
-            return 
+            return
         p= 0
         for i in range(n, 0, -1):
             if nums[i] > nums[i-1]:
                 p = i
                 break
-        print(p)
         if p == 0:
             nums.sort()
-            return 
+            return
         elif p > 0:
             j = len(nums)-1
             while nums[p-1] >= nums[j]:
                 j -= 1 
             nums[p-1], nums[j] = nums[j], nums[p-1]
-            print(nums[p:])
             nums[p:] = sorted(nums[p:])
         
             
