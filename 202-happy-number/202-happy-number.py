07564dccd89ec87090372d3a363276af1535b0be
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-01 14:22:11 -0400

    Time: 70 ms (14.64%), Space: 13.8 MB (96.69%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -1,15 +1,15 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        def gennext(n):
-            sum1 = 0
+        def helper(n):
+            ans = 0
             while n>0:
-                n, digit = divmod(n, 10)
-                sum1 += digit**2 
-            return sum1 
+                n, digit = divmod(n,10)
+                ans += digit * digit 
+            return ans 
         
         seen = set()
-        while n != 1 and n not in seen:
+        while n != 1 and n not in seen: 
             seen.add(n)
-            n = gennext(n)
+            n = helper(n)
             
         return n == 1 
