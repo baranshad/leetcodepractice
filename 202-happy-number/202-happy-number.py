eac8e915eee6f950c9375dc74d8ed30bc36526f7
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-04 21:04:09 -0400

    Time: 53 ms (49.48%), Space: 13.8 MB (96.77%) - LeetHub

--- 202-happy-number/202-happy-number.py
+++ 202-happy-number/202-happy-number.py
@@ -8,8 +8,18 @@ class Solution:
             return ans 
         
         seen = set()
-        while n != 1 and n not in seen: 
+        while n not in seen: #n != 1 and 
             seen.add(n)
             n = helper(n)
             
         return n == 1 
+    
+    
+    #take n 
+    ## 98 81 + 64 = 145 
+    ## 145 1+16+25 = 42
+    #count digits square sum of n
+    #run that in loop
+    #conditions: newN == n: return false
+    #if(newN == 1): return true
+    #newN: 
