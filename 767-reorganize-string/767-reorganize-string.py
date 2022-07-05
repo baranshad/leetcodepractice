0124952c9f02f4606a0ce0696e48a052ee58cd4e
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-04 20:35:55 -0400

    Time: 83 ms (5.24%), Space: 13.8 MB (98.60%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,29 +1,34 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        counter_str = Counter(s)
-        heap = []
-        for k, v in counter_str.items():
-            heap.append([-v, k])
-        heapq.heapify(heap)
-        ans = heap[0][1]
-        heap[0][0] += 1   
-        for i in range(len(s)-1):
-            if heap:
-                #1st max num char
-                t1 = heapq.heappop(heap) 
-                if t1[0] < 0 and t1[1] != ans[-1]: 
-                    ans += t1[1]
-                    t1[0] += 1 
-                    heapq.heappush(heap, t1)
-                elif heap:
-					#2nd max num char that does not equal to prev char
-                    t2 = heapq.heappop(heap) 
-                    #print(t2)
-                    if t2[0] < 0 and t2[1] != ans[-1]:
-                        ans += t2[1]
-                        t2[0] += 1
-                        heapq.heappush(heap, t2)
-                    heapq.heappush(heap, t1)
-                        
-        return "" if len(ans) != len(s) else ans
-                    
+        s_counter = {}
+        for i in s:
+            if i in s_counter:
+                s_counter[i] += 1 
+            else:
+                s_counter[i] = 1 
+
+        
+        #for i,j in s_counter.items():
+            #print(i,j)
+        ans = ["w"] 
+        characterWithMaxFrequency = ''
+        maxFrequency = 0
+        for i in range(len(s)):
+            #find char with max frequency
+            for char, frequency in s_counter.items():
+                if frequency > maxFrequency and char != ans[-1]:
+                    characterWithMaxFrequency = char
+                    maxFrequency = frequency
+            ## not equal ans[-1]
+            if characterWithMaxFrequency == '': return ""
+            print(characterWithMaxFrequency)
+            s_counter[characterWithMaxFrequency] = s_counter[characterWithMaxFrequency] - 1
+            ans.append(characterWithMaxFrequency)
+            maxFrequency = 0
+            characterWithMaxFrequency = ''
+            
+        #temp = 
+        return "".join(ans[1:])
+
+
+        
