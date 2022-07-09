e4ee582c41700a5cc71bb16858ef29f54e3a9af2
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-08 23:10:27 -0400

    Time: 65 ms (14.42%), Space: 13.9 MB (58.49%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -5,7 +5,7 @@ class Solution:
         else: 
             for i in range(len(digits)-1, -1, -1):
                 if digits[i] != 9: 
-                    digits[i] += 1
+                    digits[i] = digits[i]+1
                     break 
                 else:
                     digits[i] = 0
