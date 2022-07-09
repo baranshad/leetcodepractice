8fe0351a982135dc02af86c7b3dbd0d8b5949095
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-08 21:02:39 -0400

    Time: 57 ms (27.08%), Space: 14 MB (24.19%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -6,8 +6,9 @@ class Solution:
         for c in s:
             if c in leftd:
                 left.append(c)
-            elif left and c == d[left[-1]]:
-                left.pop()
             else:
-                return False         
+                if left and c == d[left[-1]]:
+                    left.pop()
+                else:
+                    return False         
         return not left  
