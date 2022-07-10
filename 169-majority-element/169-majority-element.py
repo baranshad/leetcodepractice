f6e837b4e0a644ba4aeb972da22df2a79c937bc2
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-09 21:48:37 -0400

    Time: 222 ms (65.22%), Space: 15.6 MB (34.61%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -3,9 +3,9 @@ class Solution:
         d = {}
         for i in nums:
             if i in d:
-                d[i] +=1 
+                d[i]+=1 
             else:
                 d[i] = 1 
                 
-        res = [i for i, j in d.items() if j> int(len(nums)/2)]
-        return res[0]
+        res = sorted(d.items(), key= lambda x: x[1], reverse=True)
+        return res[0][0]
