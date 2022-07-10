7bdd33c7707c85f4595aaa319452e07c5e0e00ac
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-10 18:24:00 -0400

    Time: 71 ms (15.71%), Space: 14 MB (12.55%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
@@ -0,0 +1,10 @@
+class Solution:
+    def dominantIndex(self, nums: List[int]) -> int:
+        sn = {}
+        for i, val in enumerate(nums):
+            sn[val] = i
+        lists = sorted(sn.items(), key=lambda x:x[0], reverse=True)
+        print(lists)
+        if lists[0][0] >= 2* lists[1][0]:
+            return lists[0][1] 
+        return -1
