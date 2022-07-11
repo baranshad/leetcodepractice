5986dc3f9513c26e753ec40387bb672256f3911a
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-11 18:50:54 -0400

    Time: 76 ms (11.11%), Space: 13.8 MB (54.30%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,5 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        sortedheight = sorted(heights)
+        res =[i for i, j in zip(heights, sortedheight) if i!=j]
+        return len(res)
