748c7559e7269864855ebb234eb250a000002952
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-14 21:09:21 -0400

    Time: 80 ms (5.19%), Space: 13.9 MB (57.09%) - LeetHub

---
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkRecord(self, s: str) -> bool:
+        c1 = 0
+        c2 = 0
+        maxc2  = 0
+        s1 = s + "P"
+        print(s1)
+        for i in range(len(s1)-1):
+            if s1[i] == s1[i+1] and s1[i]== "L":
+                c2 += 1 
+            else:
+                c2 = 0
+                if s1[i] == "A":
+                    c1 += 1
+            maxc2 = max(maxc2, c2+1)
+        print(maxc2)
+        return c1 < 2 and maxc2 < 3
