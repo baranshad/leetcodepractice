9d29528d6ba87b4e03c7c16fa3b27b5e20d5af5b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-16 12:52:15 -0400

    Time: 48 ms (72.19%), Space: 13.8 MB (85.55%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1copy = nums1[:m]
-        j = i = 0
-        for k in range(m+n):
-            if j >= n or (i<m and nums1copy[i] <= nums2[j]):
-                nums1[k] = nums1copy[i]
-                i += 1 
-            else:
-                nums1[k] = nums2[j]
-                j += 1 
-
+        for i in range(n):
+            nums1[i+m] = nums2[i]
+            
+        nums1.sort()
                 
     
