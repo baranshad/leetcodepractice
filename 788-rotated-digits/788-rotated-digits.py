7bf5f53cb56624d509bd5cd0dd1c83e711684b20
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-16 17:52:29 -0400

    Time: 80 ms (84.94%), Space: 13.9 MB (71.64%) - LeetHub

--- 788-rotated-digits/788-rotated-digits.py
+++ 788-rotated-digits/788-rotated-digits.py
@@ -4,27 +4,14 @@ class Solution:
         ans = 0
         for i in range(1, n+1):
             s = str(i)
-            temp = ""
+            temp =""
             for j in s:
-                if j not in d:
+                if j in d:
+                    temp += d[j]
+                else:
                     temp = ""
-                    break 
-                temp = temp + d[j]
+                    break
+            
             if temp and int(temp) != i:
                 ans += 1 
         return ans 
-    
-    
-    
-  
-        #for i in range(1,N+1):
-            #string = str(i)
-            #newstring = ""
-            #for j in string:
-                #if j not in rotate:
-                    #newstring = ""
-                    #break
-                #newstring += rotate[j]
-            #if newstring and int(newstring) != i:
-                #res += 1
-        #return res
