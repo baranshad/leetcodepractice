a55946b2fbb17c46cd0e96e60207cfb3de6ec063
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-17 15:43:38 -0400

    Time: 497 ms (19.40%), Space: 0B (100.00%) - LeetHub

---
+++ 570-managers-with-at-least-5-direct-reports/570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+select A.name 
+from Employee A 
+left join 
+Employee B 
+ on A.id = B.managerId
+ where B.managerId is not Null
+ group by B.managerId 
+ having count(distinct B.id) >= 5;  
+ 
+ 
