00a91303eecf7b83d6791463c1d5003e701098fa
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-16 21:32:04 -0400

    Time: 39 ms (83.95%), Space: 14 MB (24.29%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,29 +1,15 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        i, j, carry, res = len(a)-1, len(b)-1, 0, ""
-        while i >= 0 or j >= 0 or carry:
-            if i >= 0:
-                carry += int(a[i])
-                i -= 1
-            if j >= 0:
-                carry += int(b[j])
-                j -= 1
-            res = str(carry%2) + res
-            carry //= 2
-        return res
+        final_int, final_str, carry = str(int(a) + int(b)), "", 0
+        for i in range(len(final_int)-1, -1, -1):
+            val = (int(final_int[i]) + carry) % 2 
+            carry = (int(final_int[i]) + carry) // 2 
+            final_str += str(val)
+        if carry:
+            final_str += "1"
+        return final_str[::-1]
          
-            
-       # i, j, carry, res = len(a)-1, len(b)-1, 0, ""
-    #while i >= 0 or j >= 0 or carry:
-        #if i >= 0:
-            #carry += int(a[i])
-            #i -= 1
-        #if j >= 0:
-            #carry += int(b[j])
-            #j -= 1
-        #res = str(carry%2) + res
-        #carry //= 2
-    #return res    
+      
             
         #final_int, final_str, carry = str(int(a) + int(b)), "", 0
         
