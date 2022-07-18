cc08943dda1b30d15ff6e9a6d0783db6aad872ae
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-17 22:09:42 -0400

    Time: 55 ms (94.17%), Space: 13.9 MB (89.79%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -4,17 +4,7 @@ class Solution:
         for i in arr:
             if i*2 in d or i/2 in d:
                 return True 
-            #if i not in d:
             d.add(i)
         return False 
     
-     
-     
-       #arrDict ={}		
-        #for num in arr:
-            #if num * 2 in arrDict or num/2 in arrDict:
-                #return True
-            
-            #if num not in arrDict:
-                #arrDict[num] = None
-        #return False
+      
