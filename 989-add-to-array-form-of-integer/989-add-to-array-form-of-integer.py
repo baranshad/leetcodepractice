64ae6bdc4e8bf2614155302b54ae1de85b689e30
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-19 17:30:21 -0400

    Time: 319 ms (86.42%), Space: 14.6 MB (87.99%) - LeetHub

--- 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
+++ 989-add-to-array-form-of-integer/989-add-to-array-form-of-integer.py
@@ -1,6 +1,19 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        numint = "".join(str(i) for i in num)
-        res = int(numint) + k 
-        ans = [int(i) for i in str(res)]
-        return ans 
+        if len(num) < len(str(k)):
+            for i in range(len(str(k))- len(num)):
+                num.insert(0,0)
+                
+        res = ""
+        carry = 0
+        for i in range(len(num)-1, -1, -1):
+            curval = 0
+            val = (num[i] + k % 10 + carry) % 10 
+            carry = (num[i] + k % 10 + carry) // 10 
+            res += str(val)
+            curval = val 
+            k = k//10 
+        if carry:
+            res += "1"
+        return list(res[::-1])
+            
