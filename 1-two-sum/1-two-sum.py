a73a9487a7997aabbc2c03f1654bb2a395e64d3b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-18 22:13:29 -0400

    Time: 89 ms (70.50%), Space: 15.3 MB (24.50%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,14 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        d = {}
+        d= {}
         for i, val in enumerate(nums):
             n = target - val 
-            if n in d:# 这一步很重要，如果没有这一步，
-                ## 【3，2，4】 的情况，会出现【0，0】的结果，当d = {3: 0} 之后
-                ## 这时候 6-3 = 3， 出现在d里面，会返回 【0，0】
-                return [d[n],i]
-            
+            if n in d:
+                return [d[n], i]
             else:
-                d[val] = i
+                d[val] = i 
                 
-      
+        
