82c16bac07d380b7e304174a52be199b0473eec2
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-24 13:09:12 -0400

    Time: 94 ms (42.05%), Space: 14 MB (95.25%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -2,13 +2,12 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         if not nums1 or not nums2:
             return 
-        
         stack = []
         d= {}
         for i in  nums2:
             while stack and i > stack[-1]:
                 d[stack.pop()] = i
-            stack.append(i)
-            
+                #print(d)
+            stack.append(i)  
         res = [d.get(i, -1) for i in nums1]
         return res 
