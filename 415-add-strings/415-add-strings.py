12932d9570c96cd6b03dbc1aa21fce84b5571bd0
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-24 15:48:48 -0400

    Time: 92 ms (17.26%), Space: 14.4 MB (6.64%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -4,19 +4,17 @@ class Solution:
         i = len(num1)-1
         j = len(num2)-1
         res = []
-        while i >= 0 or j >= 0:
+        while i>=0  or j>=0 :
             x1 = int(num1[i]) if i >= 0 else 0
             x2 = int(num2[j]) if j >= 0 else 0
-            value = (x1 + x2 + carry) % 10 
-            carry = (x1 + x2 + carry) // 10
-            res.append(value)
+            val = (x1+x2+carry) % 10 
+            carry = (x1+x2+carry) // 10 
+            res.append(val)
             i -= 1 
             j -= 1 
-            
         if carry:
             res.append(carry)
+        return "".join(str(i) for i in res[::-1])
+    
+     
             
-            
-        return "".join(str(x) for x in res[::-1])
-            
-        
