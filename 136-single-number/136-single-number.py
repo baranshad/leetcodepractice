15a239d7ad01f165344996c5b1b18e5197b620b1
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-24 21:27:51 -0400

    Time: 148 ms (85.18%), Space: 16.7 MB (84.32%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,11 +1,5 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = {}
-        for i in nums:
-            if i not in d:
-                d[i] = 1 
-            else:
-                d[i] += 1 
-        
-        ans = [i for (i,j) in d.items() if j==1]
-        return ans[0]
+        s = Counter(nums)
+        ss = sorted(s.items(), key = lambda x: x[1])
+        return ss[0][0]
