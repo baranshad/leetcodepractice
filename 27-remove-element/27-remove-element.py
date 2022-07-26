e2050b36c9e275de823a30188dcfe7329b06f669
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-25 21:42:38 -0400

    Time: 34 ms (93.46%), Space: 13.8 MB (97.17%) - LeetHub

--- 27-remove-element/27-remove-element.py
+++ 27-remove-element/27-remove-element.py
@@ -3,7 +3,7 @@ class Solution:
         i = 0
         for j in range(len(nums)):
             if nums[j] != val:
-                nums[i], nums[j] = nums[j], nums[i]
+                nums[i] = nums[j]
                 i += 1 
         return i 
     
