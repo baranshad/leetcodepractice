e993eadb2ebe2ccc9eb481431d7ad99cef7d18f4
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-27 20:10:09 -0400

    Time: 67 ms (28.86%), Space: 13.9 MB (83.08%) - LeetHub

--- 819-most-common-word/819-most-common-word.py
+++ 819-most-common-word/819-most-common-word.py
@@ -1,24 +1,13 @@
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
-        res = ""
-        max_count = 0
-        d = defaultdict(int)
-        buffer = []
-        banned_set = set(banned)
-        for i, char in enumerate(paragraph):
-            if char.isalnum():
-                buffer.append(char.lower())
-                if i != len(paragraph) -1 :
-                    continue 
-                    
-            if len(buffer) > 0:
-                word = "".join(buffer)
-                if word not in banned_set:
-                    d[word] +=1 
-                    if d[word] > max_count:
-                        max_count = d[word]
-                        res = word 
-            buffer = []
-        return res 
-                        
+        set_banned = set(banned)
+        para_lower = paragraph.lower()
+        para_lower = re.sub(r"[!?',;.]+",' ',para_lower)
+        word_list = para_lower.split()
+        counter = Counter(word_list).most_common()
+        for word,_ in counter:
+            if word not in set_banned:
+                result = word
+                break
+        return result
             
