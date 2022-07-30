fcf6894e97e9988e7370bc05ad020f593a89f69b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-07-29 21:48:40 -0400

    Time: 4006 ms (5.04%), Space: 18.6 MB (7.51%) - LeetHub

--- 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/501-find-mode-in-binary-search-tree.py
@@ -6,28 +6,22 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        final_mode, max_freq = root.val, 1
-        dict_nums = {}
+        res = 1 
+        d = {}
+        def check(node, d):
+            if not node:
+                return d 
+            if node.val in d:
+                d[node.val] += 1 
+            else:
+                d[node.val] = 1 
+                
+            if node.left:
+                check(node.left, d)
+            if node.right:
+                check(node.right, d)
+                
+            return d 
         
-        def check_tree(node, dict_nums):
-            if not node: return dict_nums
-            
-            if node.val in dict_nums: dict_nums[node.val] += 1
-            else: dict_nums[node.val] = 1
-            
-            if node.left: check_tree(node.left, dict_nums)
-            if node.right: check_tree(node.right, dict_nums)
-            
-            return dict_nums
-            
-        check_tree(root, dict_nums)
-        max_num = max(dict_nums.values())
-        '''
-        ans = []
-        for i, val in dict_nums.items():
-            if val == max_num:
-                ans.append(i)
-        return ans
-        '''
-        
-        return (i for i,val in dict_nums.items() if(val == max_num))
+        check(root, d)
+        return [i for i,j in d.items() if j == max(d.values())]
