5249e9f7abfe63858c0c68a2618f9ab9a3266d85
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-01 23:04:39 -0400

    Time: 1062 ms (7.18%), Space: 0B (100.00%) - LeetHub

--- 177-nth-highest-salary/177-nth-highest-salary.sql
+++ 177-nth-highest-salary/177-nth-highest-salary.sql
@@ -4,8 +4,8 @@ BEGIN
       # Write your MySQL query statement below.
       select max(salary) from 
       employee e1 
-      where 
-      (select count(distinct salary) = N-1 
+      where N-1 = 
+      (select count(distinct salary) 
       from employee e2 
       where e2.salary > e1.salary)
       
