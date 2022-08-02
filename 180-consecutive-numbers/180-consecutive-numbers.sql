c4a7799ab96492274a10ba17b26543474fb7676f
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-01 23:26:29 -0400

    Time: 1288 ms (10.26%), Space: 0B (100.00%) - LeetHub

--- 180-consecutive-numbers/180-consecutive-numbers.sql
+++ 180-consecutive-numbers/180-consecutive-numbers.sql
@@ -4,6 +4,6 @@ l1.num as ConsecutiveNums
 from 
 logs l1 
 inner join 
-logs l2 on l2.id = l1.id+1 and l1.num = l2.num 
+logs l2 on l2.id = l1.id+1 and l2.num = l1.num 
 inner join 
-logs l3 on l3.id = l1.id+2 and l1.num = l3.num
+logs l3 on l3.id = l1.id+2 and l3.num = l1.num
