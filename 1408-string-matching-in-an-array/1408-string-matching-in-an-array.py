fe8c9ae4a1666c6dd64e01c2a9c58a7fb75b6a79
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-03 23:12:09 -0400

    Time: 47 ms (81.99%), Space: 13.7 MB (96.51%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.py
@@ -2,12 +2,11 @@ class Solution:
     def stringMatching(self, words: List[str]) -> List[str]:
         res = set()
         words.sort(key=len)
-        for i, j in enumerate(words):
+        for i, val in enumerate(words):
             for word in words[i+1:]:
-                if j in word:
-                    res.add(j)
+                if val in word:
+                    res.add(val)
         return list(res)
-        
         
         
         #ret = set()
