1f5df6d72b8fa6079a3a92b472c8a1f09e3cece6
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-05 18:17:34 -0400

    Time: 47 ms (92.35%), Space: 14.2 MB (61.20%) - LeetHub

--- 893-groups-of-special-equivalent-strings/893-groups-of-special-equivalent-strings.py
+++ 893-groups-of-special-equivalent-strings/893-groups-of-special-equivalent-strings.py
@@ -2,7 +2,8 @@ class Solution:
     def numSpecialEquivGroups(self, words: List[str]) -> int:
         helper = defaultdict(int)
         for string in words:
-            key = "".join(sorted(string[0::2])+sorted(string[1::2]))
-            helper[key] += 1
+            tempword = "".join(sorted(string[0::2]) + sorted(string[1::2]))
+            helper[tempword] += 1 
         return len(helper)
+            
      
