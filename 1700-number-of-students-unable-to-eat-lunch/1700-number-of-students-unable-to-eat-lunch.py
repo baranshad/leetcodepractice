743feb9d97a4134dceab641afe78e3adf48facf4
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-04 22:55:31 -0400

    Time: 54 ms (64.83%), Space: 13.9 MB (22.74%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -1,18 +1,21 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        i = 0 
+        i = 0
         while i < len(students):
             if students[0] == sandwiches[0]:
                 students.pop(0)
                 sandwiches.pop(0)
-                i = 0
+                i= 0
             else:
                 students.append(students.pop(0))
-                i+= 1 
+                i+=1 
             if i == len(students):
-                return len(students)
-        
-        
-        
+                return i 
+
+            # This block of code handles both scenarios at the end of each iteration:
+            #           1) All students currently in the queue are unable to eat.
+            #           2) Every student has taken a sandwich.
+
+
         
         
