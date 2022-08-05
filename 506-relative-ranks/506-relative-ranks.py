ea3a44baa810d10a4d64998c5fa74c279109f8b3
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-05 17:21:24 -0400

    Time: 93 ms (81.44%), Space: 15.1 MB (73.17%) - LeetHub

--- 506-relative-ranks/506-relative-ranks.py
+++ 506-relative-ranks/506-relative-ranks.py
@@ -3,13 +3,14 @@ class Solution:
         j = 1 
         d = {}
         for i in reversed(sorted(score)):
-            if j == 1:
+            if j ==1:
                 d[i] = "Gold Medal"
             elif j==2:
                 d[i] = "Silver Medal"
-            elif j ==3:
+            elif j==3:
                 d[i] = "Bronze Medal"
             else:
                 d[i] = str(j)
-            j += 1
+            j += 1 
+                
         return [d[i] for i in score]
