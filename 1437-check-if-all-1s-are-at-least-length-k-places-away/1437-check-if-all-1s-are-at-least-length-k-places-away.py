c1adac20a0814e050c4bf38bee053c3c76f4fad0
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 15:26:13 -0400

    Time: 603 ms (88.09%), Space: 16.8 MB (57.79%) - LeetHub

--- 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away/1437-check-if-all-1s-are-at-least-length-k-places-away.py
@@ -2,21 +2,13 @@ class Solution:
     def kLengthApart(self, nums: List[int], k: int) -> bool:
         count = k 
         for i in nums:
-            if i == 1: 
-                if count < k:
+            if i==1:
+                if count<k:
                     return False 
-                count = 0 
+                count = 0
             else:
                 count += 1 
-        return True 
                 
-               
+        return True
                 
-        #prev = -inf       
-        #for idx, val in enumerate(nums):
-            #if val == 1:
-                #if idx - prev <= k:
-                    #return False
-                #else:
-                    #prev = idx
-        #return True
+        
