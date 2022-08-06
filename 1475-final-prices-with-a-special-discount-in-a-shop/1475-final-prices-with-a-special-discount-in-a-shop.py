09c7384a4b9d0f511e0afc796582bf684633c9c3
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-05 23:00:34 -0400

    Time: 55 ms (91.60%), Space: 13.9 MB (65.41%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -2,14 +2,11 @@ class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
         res = []
         ans = [0]*len(prices)
-        for i in reversed(range(len(prices))):
+        for i in range(len(prices)-1, -1, -1):
             while res and prices[i] < res[-1]:
                 res.pop()
-            ans[i]= prices[i] -res[-1] if res else prices[i] 
-            res.append(prices[i] )
-            
+            ans[i] = prices[i] - res[-1] if res else prices[i]
+            res.append(prices[i])
         return ans 
-        
-        
       
         
