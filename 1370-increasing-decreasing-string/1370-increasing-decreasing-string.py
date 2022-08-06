194f66047d022ef0132fabc7d40fa4bbf2e5020d
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 14:50:36 -0400

    Time: 136 ms (29.11%), Space: 13.8 MB (85.07%) - LeetHub

--- 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.py
@@ -2,23 +2,22 @@ class Solution:
     def sortString(self, s: str) -> str:
         c = Counter(s)
         alphabets = "abcdefghijklmnopqrstuvwxyz"
-        rev_alphabets = alphabets[::-1]
+        reverse_alpha = alphabets[::-1]
         res = ""
         count = len(s)
-        while count >0:
+        while count > 0:
             for i in alphabets:
                 if c[i]:
-                    res+= i 
+                    count -=1 
+                    res += i 
                     c[i] -= 1 
-                    count -= 1 
-            for j in rev_alphabets:
+            for j in reverse_alpha:
                 if c[j]:
-                    res+=j 
-                    c[j] -= 1 
-                    count -= 1 
+                    count -=1 
+                    res+= j
+                    c[j] -=1 
         return res 
-            
-            
-            
+
+                    
             
     
