ec2c96c5c359ddc8505b93c9c12c5dacf433ba51
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 16:00:59 -0400

    Time: 24 ms (99.51%), Space: 13.9 MB (21.13%) - LeetHub

--- 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
+++ 1576-replace-all-s-to-avoid-consecutive-repeating-characters/1576-replace-all-s-to-avoid-consecutive-repeating-characters.py
@@ -1,27 +1,16 @@
 class Solution:
     def modifyString(self, s: str) -> str:
-        ans=list(s)  
-        abc = set(['a','b','c'])
-        for i, c in enumerate(ans):
-            if c == "?":
-                neib = set()
+        ans = list(s)
+        use3 = ["a","b","c"]
+        for i, char in enumerate(ans):
+            if char == "?":
+                temp = set()
                 if i != 0:
-                    neib.add(ans[i-1])
-                if i != len(ans)-1:
-                    neib.add(ans[i+1])
-                ans[i]=(abc - neib).pop()
-        
-        return "".join(ans)
-    
-    
-        #s = list(s)
-        #abc = set(['a','b','c'])
-        #for i, c in enumerate(s):
-            #if c == '?':
-                #neib = set()
-                #if i != 0:
-                    #neib.add(s[i-1])
-               # if i != len(s)-1:
-                    #neib.add(s[i+1])
-                #s[i] = (abc-neib).pop()
-        #return "".join(s)
+                    temp.add(ans[i-1])
+                if i != len(s)-1:
+                    temp.add(ans[i+1])
+                ans[i] = (set(use3)- temp).pop()
+                
+        return "".join(ans) 
+   
+       
