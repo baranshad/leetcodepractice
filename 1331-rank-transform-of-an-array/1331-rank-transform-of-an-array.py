03650bb2a3ade11766673535d7a077b43216fc8a
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 14:15:50 -0400

    Time: 372 ms (91.09%), Space: 31.4 MB (60.69%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        d = {}
+        arr_sort= sorted(arr)
+        rank = 1 
+        for i in arr_sort:
+            if i in d:
+                continue
+            else:
+                d[i] = rank 
+                rank = rank +1 
+        print(d)
+        return [d[i] for i in arr]
