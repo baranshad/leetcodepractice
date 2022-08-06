1777fff65794448f09389ce424d468c0f3c0cb2a
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 13:37:28 -0400

    Time: 50 ms (55.25%), Space: 13.9 MB (13.37%) - LeetHub

--- 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.py
+++ 1217-minimum-cost-to-move-chips-to-the-same-position/1217-minimum-cost-to-move-chips-to-the-same-position.py
@@ -2,9 +2,8 @@ class Solution:
     def minCostToMoveChips(self, position: List[int]) -> int:
         e = o = 0
         for i in position:
-            if i % 2 == 0:
-                e += 1 
+            if i %2 ==0:
+                e+=1 
             else:
                 o += 1 
-                
         return min(e,o)
