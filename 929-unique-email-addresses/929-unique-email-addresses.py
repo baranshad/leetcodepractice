dfdf429d7b02484804ced0e335c8756d17a302e3
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-05 22:08:33 -0400

    Time: 93 ms (40.66%), Space: 13.9 MB (97.63%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.py
+++ 929-unique-email-addresses/929-unique-email-addresses.py
@@ -2,23 +2,9 @@ class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         res = set() 
         for word in emails:
-            temp = ""
-            for i, char in enumerate(word):
-                if char.isalpha():
-                    temp += char 
-                elif char == ".":
-                    continue
-                elif char =="+" or char == "@":
-                    break 
-                    
-                    
-            temp1 = ""
-            for i, char in enumerate(reversed(word)):
-                temp1 += char
-                if char == "@":
-                    break 
-                    
-            cur = temp + temp1[::-1]
-            res.add(cur)
+            name, domain = word.split('@')
+            local = name.split('+')[0].replace('.','')
+            res.add(local+'@'+domain)
+            
         return len(res)
                 
