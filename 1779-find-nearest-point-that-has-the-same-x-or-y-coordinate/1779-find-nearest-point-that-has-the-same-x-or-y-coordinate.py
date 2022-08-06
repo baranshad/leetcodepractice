5e756b9912a775a497301ddaeaff0e7957c7f4eb
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 18:30:50 -0400

    Time: 813 ms (81.32%), Space: 19.2 MB (66.59%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.py
@@ -3,30 +3,14 @@ class Solution:
         mind = inf
         idx = cur = 0
         for i in range(len(points)):
-            if points[i][0] == x or points[i][1] == y: 
-                cur = abs(x-points[i][0]) + abs(y-points[i][1])
-                #print(cur)
+            if points[i][0] == x or points[i][1] == y:
+                cur = abs(points[i][0]-x) + abs(points[i][1]- y)
                 if cur < mind:
                     mind = cur 
                     idx = i 
+                    
         if mind == inf:
             return -1 
         else:
             return idx 
-        #print(idx)
         
-        
-        #cur_index = 0
-        #cur_min_man = 99999
-        #man = 0
-        #for i in range(len(points)):
-            #if x == points[i][0] or y == points[i][1]:
-                #val = (abs(x - points[i][0]) + abs(y - points[i][1]))
-                #if cur_min_man > val:
-                    #cur_min_man = val
-                    #cur_index = i
-                
-        #if cur_min_man == 99999:
-            #return (-1)
-        #else:
-            #return (cur_index)
