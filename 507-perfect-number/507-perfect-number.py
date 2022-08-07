2d45eb89bdd5dcdae5fef497977a3f4af5d03775
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-07 11:21:06 -0400

    Time: 70 ms (34.53%), Space: 14 MB (9.16%) - LeetHub

--- 507-perfect-number/507-perfect-number.py
+++ 507-perfect-number/507-perfect-number.py
@@ -3,12 +3,12 @@ class Solution:
         if num <= 0:
             return False
         
-        factors = list(set(x for tup in ([i, num//i] for i in range(1, int(num**0.5)+1) if num % i == 0) for x in tup))
-        sumfactors = sum(factors) - num
-        if num % 2 != 0:
-            return False
-        else:
-            if num == sumfactors:
-                return True
-            else:
-                return False
+        #factors = list(set(x for tup in ([i, num//i] for i in range(1, int(num**0.5)+1) if num % i == 0) for x in tup))
+        factorset = set()
+        for i in range(1, int(num**0.5)+1):
+            if num % i ==0:
+                factorset.add(i)
+                factorset.add(num//i)
+        
+        factorset.remove(num)
+        return sum(factorset) == num
