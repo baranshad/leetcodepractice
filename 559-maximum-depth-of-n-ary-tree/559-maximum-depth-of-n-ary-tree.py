87ca68c4e6a54f0dfa3b6c259d1c5d6e1ee8373b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-07 11:27:56 -0400

    Time: 52 ms (85.78%), Space: 16 MB (93.83%) - LeetHub

--- 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.py
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.py
@@ -10,16 +10,17 @@ class Solution:
     def maxDepth(self, root: 'Node') -> int:
         if not root:
             return 0 
+        
         stack = []
-        if root is not None:
-            stack.append((1, root))
-            
-        depth = 0 
+        stack.append((1,root))
+        depth = 0
         while stack:
-            curdepth, root = stack.pop()
-            if root:
-                depth = max(depth, curdepth)
-                for c in root.children:
-                    stack.append((curdepth +1, c))
+            currentdepth, node = stack.pop()
+            if node:
+                depth = max(depth, currentdepth)
+                for c in node.children:
+                    stack.append((currentdepth+1, c))
                     
         return depth 
+                
+        
