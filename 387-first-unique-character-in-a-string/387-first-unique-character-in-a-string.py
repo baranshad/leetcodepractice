735f13114990803fb8710b4bafe5c241a0007291
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 21:26:40 -0400

    Time: 104 ms (88.55%), Space: 14.2 MB (59.00%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -4,10 +4,8 @@ class Solution:
         ans = [i for i, j in d.items() if j ==1]
         if ans == []:
             return -1
-        #idx = len(s)
         for i, char in enumerate(s):
             if char in ans:
-                #idx = min(idx, i)  
                 return i 
                 break 
         return -1
