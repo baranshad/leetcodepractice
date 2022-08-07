1afdc24772e6044c205b2ec08860254fa18eff0b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-06 23:05:09 -0400

    Time: 444 ms (36.28%), Space: 15.4 MB (33.73%) - LeetHub

--- 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
+++ 453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.py
@@ -1,9 +1,4 @@
 class Solution:
     def minMoves(self, nums: List[int]) -> int:
-        moves = 0
-        minvalue = float(inf)
-        for i in nums:
-            moves += i 
-            minvalue = min(minvalue, i)
-            
-        return moves - minvalue * len(nums)
+        sumall = sum(nums)
+        return sumall - len(nums)*(min(nums))
