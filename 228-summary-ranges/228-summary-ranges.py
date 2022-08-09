542bb327146b70faddeb4f105bea421d4d46f3f1
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-08 20:26:05 -0400

    Time: 27 ms (97.94%), Space: 13.8 MB (71.22%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.py
+++ 228-summary-ranges/228-summary-ranges.py
@@ -3,12 +3,15 @@ class Solution:
         ans = []
         i = 0
         for j in range(len(nums)):
-            if j == len(nums) -1 or nums[j+1]- nums[j] >1 :
+            if j == len(nums) -1 or nums[j+1] - nums[j] > 1:
                 if i != j:
-                    ans.append(str(nums[i]) + "->" + str(nums[j]))
-                elif i ==j:
+                    ans.append(str(nums[i]) + "->" + str(nums[j]))   
+                else:
                     ans.append(str(nums[j]))
-                i = j+1 
+            
+            
+                i = j + 1 
+            
         return ans 
             
             
