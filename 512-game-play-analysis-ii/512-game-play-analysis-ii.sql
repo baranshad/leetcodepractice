5c5188073d4a24e6959e255d57127585692729df
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-08 22:55:47 -0400

    Time: 633 ms (57.02%), Space: 0B (100.00%) - LeetHub

---
+++ 512-game-play-analysis-ii/512-game-play-analysis-ii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select distinct 
+player_id, device_id 
+from Activity 
+where(player_id, event_date) in 
+(select player_id, min(event_date) from Activity 
+group by player_id)
+
+ 
