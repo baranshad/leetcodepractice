d033f4525fbe397a5e14c260d49f46b0d23c9e0d
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-08 21:11:12 -0400

    Time: 54 ms (72.53%), Space: 13.9 MB (76.12%) - LeetHub

--- 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
+++ 1640-check-array-formation-through-concatenation/1640-check-array-formation-through-concatenation.py
@@ -1,14 +1,18 @@
 class Solution:
     def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
-        n = len(arr)
-        i = 0
-        mapping = {d[0]: d for d in pieces}
-        while i < n:
-            if arr[i] not in mapping:
+        d = defaultdict(list)
+        for i in pieces:
+            d[i[0]] = i 
+            
+        j = 0 
+        while j < len(arr):
+            if arr[j] not in d:
                 return False 
             else:
-                for x in mapping[arr[i]]:
-                    if x != arr[i]:
+                for x in d[arr[j]]:
+                    if x!= arr[j]:
                         return False 
-                    i += 1 
+                    j += 1 
         return True 
+                
+            
