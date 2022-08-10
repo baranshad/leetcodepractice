6bfaa41ef13fb29dc09e2d6b31e88c797a861601
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-09 22:19:48 -0400

    Time: 37 ms (81.41%), Space: 14 MB (11.22%) - LeetHub

--- 551-student-attendance-record-i/551-student-attendance-record-i.py
+++ 551-student-attendance-record-i/551-student-attendance-record-i.py
@@ -1,19 +1,16 @@
 class Solution:
     def checkRecord(self, s: str) -> bool:
-        numA = numL = maxA = 0
-        s = s + "P"
+        numA = numL = ans = 0
+        s += "P"
+        
         for i in range(len(s)-1):
-            if s[i] == s[i+1] and s[i] == "L":
+            if s[i+1] == s[i] and s[i]=="L":
                 numL += 1 
             else:
-                numL = 0 
+                numL = 0
                 if s[i] == "A":
                     numA += 1 
-            
-            maxA = max(numL+1, maxA)
-            
-        return maxA < 3 and numA < 2 
-                
-                
-            
-            
+                    
+            ans = max(numL+1, ans)
+        
+        return ans < 3 and numA < 2 
