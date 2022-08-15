ff0607e72f1f345013ecfc5ae0d5cf8925c4b9b0
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-14 22:10:44 -0400

    Time: 7954 ms (29.73%), Space: 14.2 MB (43.69%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -3,16 +3,18 @@ class Solution:
         ans = float("inf")
         nums.sort()
         for i in range(len(nums)):
-            l = i+1
-            r = len(nums) -1
+            cur = nums[i]
+            l = i+1 
+            r = len(nums)-1 
             while l < r:
-                sum3 = nums[i] + nums[l] + nums[r]
-                if abs(target - sum3) < abs(ans):
+                sum3 = cur + nums[l] + nums[r]
+                if abs(sum3 - target) < abs(ans):
                     ans = target - sum3 
                 if sum3 < target:
                     l += 1 
                 else:
                     r -= 1 
-            if ans ==0:
+            if ans == 0:
                 break 
-        return target - ans
+                
+        return target - ans 
