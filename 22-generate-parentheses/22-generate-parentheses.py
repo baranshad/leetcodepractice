aaba5fb053418fa0121a2029c2612f46eef5fb69
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-14 23:51:14 -0400

    Time: 57 ms (47.27%), Space: 14.2 MB (40.06%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -1,10 +1,9 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        def helper(s=[], l=0, r=0):
-            if len(s) ==2*n:
+        def helper(s, l, r):
+            if len(s) == 2*n:
                 ans.append("".join(s))
-                
             if l < n:
                 s.append("(")
                 helper(s, l+1, r)
@@ -15,6 +14,8 @@ class Solution:
                 helper(s, l, r+1)
                 s.pop()
                 
-        helper()
+            return ans 
+        
+        helper([], 0,0)
         return ans 
-                    
+                
