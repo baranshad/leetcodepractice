6aadd089aef994e0dfd07511f71280f8cc888d5b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-14 23:44:47 -0400

    Time: 90 ms (97.96%), Space: 14 MB (33.64%) - LeetHub

--- 18-4sum/18-4sum.py
+++ 18-4sum/18-4sum.py
@@ -4,7 +4,7 @@ class Solution:
             res = []
             s = set()
             for i in range(len(nums)):
-                if len(res) == 0 or res[-1][1] != nums[i]:
+                if len(res) == 0 or nums[i] != res[-1][1]:
                     if target - nums[i] in s:
                         res.append([target-nums[i], nums[i]])
                     s.add(nums[i])
@@ -12,18 +12,17 @@ class Solution:
         
         def ksum(nums, target, k):
             res = []
+            
             if not nums:
                 return res 
-            ave = target //k 
+            ave = target // k 
             if ave < nums[0] or nums[-1] < ave:
                 return res 
             
-            if k == 2:
-                return twosum(nums, target)
-            
+            if k == 2: return twosum(nums, target)
             for i in range(len(nums)):
-                if i == 0 or nums[i-1] != nums[i]:
-                    for subset in ksum(nums[i+1:], target - nums[i], k-1):
+                if i ==0 or nums[i-1] != nums[i]:
+                    for subset in ksum(nums[i+1:], target- nums[i], k-1):
                         res.append([nums[i]] + subset)
                         
             return res 
