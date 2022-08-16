982b2d7077b305c6e38e0cf4c7d027d24bb2bbe5
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-15 22:42:39 -0400

    Time: 78 ms (26.10%), Space: 13.9 MB (25.55%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -1,33 +1,26 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        ## find the first updown pattern
-        ## from the right to left, cur > left and cur > right 
-        ## if cur go until 0, then just return sorted(reverseorder)
-        n = len(nums)-1
-        if n == 0: 
-            return nums
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums) -1 
+        if n == 0:
+            return nums 
         if nums[-1] > nums[-2]:
-            nums[-1],nums[-2] = nums[-2],nums[-1]
-            return
-        p= 0
+            nums[-1], nums[-2] = nums[-2], nums[-1]
+            return 
+        p= 0 
         for i in range(n, 0, -1):
             if nums[i] > nums[i-1]:
-                p = i
-                break
+                p = i 
+                break 
         if p == 0:
             nums.sort()
-            return
+            return 
         elif p > 0:
-            j = len(nums)-1
+            j = len(nums) -1 
             while nums[p-1] >= nums[j]:
                 j -= 1 
             nums[p-1], nums[j] = nums[j], nums[p-1]
             nums[p:] = sorted(nums[p:])
-        
-            
-        
-        
-        
-    
-
-  
+                
