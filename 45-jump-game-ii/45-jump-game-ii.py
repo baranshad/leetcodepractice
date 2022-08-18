c695d4be300fa89595373e503d262863ac520a0b
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-17 21:41:54 -0400

    Time: 204 ms (65.47%), Space: 15 MB (90.82%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,18 +1,9 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # find the first position with nums[i] + i >= len(nums)
-        # most attainable position 
-        # 0: [1] 1: [2] 2:[3,4] 3 [4,5] find the idx 3, no need to go to idx 4 
-        # just return len(d)
-        # if can jump to nums[i] will always can jump to nums[i-1]
-        start = 0
-        end = 1
+        i = 0 
+        j = 1 
         count = 0
-        
-        while end <= len(nums)-1:
-            start, end = end, max(nums[i]+i+1 for i in range(start, end))
-            count += 1
-            
-        return count    
-                
-                
+        while j < len(nums):
+            i, j = j, max(nums[i]+ i +1 for i in range(i,j))
+            count += 1 
+        return count 
