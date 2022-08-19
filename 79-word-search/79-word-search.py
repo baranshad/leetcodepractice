3dea99d34271f31fcd4a5cbb0e5b5cffcbfedd39
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-18 23:35:23 -0400

    Time: 5593 ms (80.42%), Space: 14 MB (51.29%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,40 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
         m = len(board)
         n = len(board[0])
         
-        
-        def helper(i,j, wordlist):
+        def helper(i, j, wordlist):
             if not len(wordlist):
-                return True
-            
-            if i < 0 or j < 0 or i>=m or j >= n or wordlist[0] != board[i][j]:
+                return True 
+            if i < 0 or j < 0 or i >= m or j >= n or wordlist[0] != board[i][j] :
                 return False 
-            
-            board[i][j] ="#"
+           
+            board[i][j] = "#"
             for dx, dy in [(0,1),(1,0),(-1,0),(0,-1)]:
                 nx, ny = i+dx, j+dy 
                 if helper(nx, ny, wordlist[1:]):
                     return True 
             board[i][j] = wordlist[0]
             return False 
-            
-            
-        
-        #print(helper(0,0, list(word), set()))
         
 
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
                     if helper(i,j, word):
-                        #print(helper(i,j,[], list(word)))
                         return True 
         return False 
-                    
-                    
- 
-  
-                
-                
+            
