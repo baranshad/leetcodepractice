8db132479cc4d2c093c9f77cb605cd36e32ecbe5
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-20 18:01:43 -0400

    Time: 936 ms (5.00%), Space: 16 MB (67.41%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -5,14 +5,21 @@ class Solution:
             d[i].append(j)
             
         for k in range(numCourses):
-            q = deque(d[k])
+            q = deque(d[k]) # 1,2,3,0
             seen = set()
-            while q:
-                m = q.pop()
-                if m == k:
+            while q: ## 3
+  # [[0,2], [0,1],[2,1]] is true 
+  ##.  0 
+  ## 1   2 
+  ##     1  if here 1 can back to 0 then cycle 
+                m = q.pop()  
+                if m == k: # if k = 0, then when met 0 there is a cycle 
                     return False 
-                seen.add(m)
-                for nb in d[m]:
-                    if nb not in seen:
+                seen.add(m)  ## if no circle, add to seen classes 
+                for nb in d[m]:  ## go dfs, to the nbs of the class
+                    #print(seen, nb)
+                    if nb not in seen: ## if not seen then append 
                         q.append(nb)
+                        #print(q)
+            print(seen)
         return True 
