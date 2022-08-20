0049fcb5b43017ad2ecfe6a9dd704a76ce8488be
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-19 20:35:57 -0400

    Time: 79 ms (66.51%), Space: 14.9 MB (82.31%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -8,13 +8,12 @@ class Solution:
             if s_list[left] in d:
                 if s_list[right] in d:
                     s_list[left], s_list[right] = s_list[right], s_list[left]
-                    left +=1 
-                    right -=1 
+                    left += 1 
+                    right -= 1 
                 else:
                     right -= 1 
             else:
                 left += 1 
                 
         return "".join(s_list)
-        
        
