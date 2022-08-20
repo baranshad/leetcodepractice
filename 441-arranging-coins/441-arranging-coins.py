37d1237dd648482dfb2421bd614d9f6efcc56f03
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-19 22:12:26 -0400

    Time: 1889 ms (10.94%), Space: 14 MB (12.15%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.py
+++ 441-arranging-coins/441-arranging-coins.py
@@ -3,22 +3,9 @@ class Solution:
         count = 0
         rowindex = 1 
         while n > 0:
-            n = n - rowindex 
+            n -= rowindex 
             rowindex += 1 
-            if n >=0:
+            if n >= 0:
                 count += 1 
-        return count
-            
-            
-            
-        #result = 0
-
-		#stairs_number = 1
-
-		#while n > 0:
-			#n = n - stairs_number
-			#stairs_number = stairs_number + 1
-			#if n >= 0:
-				#result = result + 1
-
-		#return result
+                
+        return count 
