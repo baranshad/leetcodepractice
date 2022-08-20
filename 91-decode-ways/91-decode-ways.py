4287a8337f15faa02a6a9783347a2a2d0af3919c
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-19 20:20:05 -0400

    Time: 41 ms (76.69%), Space: 14.1 MB (20.11%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -3,19 +3,17 @@ class Solution:
         @lru_cache(maxsize=None)
         def helper(index, s):
             if index == len(s):
-                return 1
-
+                return 1 
             if s[index] == '0':
-                return 0
-
-            if index == len(s)-1:
-                return 1
-        
-            answer = helper(index + 1, s)
-            if int(s[index : index + 2]) <= 26:
-                answer += helper(index + 2, s)
-
-            return answer
+                return 0 
+            if index == len(s) -1:
+                return 1 
+            
+            ans  = helper(index+1, s)
+            if int(s[index:index+2]) <= 26:
+                ans += helper(index+2, s)
+                
+            return ans 
         
         
         return helper(0, s)
