86a77976ebb752eb4dde0dddaf4ce09dd978f6b8
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-20 17:09:19 -0400

    Time: 534 ms (5.00%), Space: 15.6 MB (71.04%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,20 +1,18 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        d = defaultdict(list) # create adjacency list
+        d = defaultdict(list)
         for i, j in prerequisites:
             d[i].append(j)
-                        
-        for n in range(0,numCourses):
-            Q = deque(d[n])
-            visited = set()
-            while Q:
-                #m = Q.popleft()  # --> BFS
-                m = Q.pop()     # --> DFS
-                if m==n:
-                    return False # we detected a loop
-                visited.add(m)  
+            
+        for k in range(numCourses):
+            q = deque(d[k])
+            seen = set()
+            while q:
+                m = q.pop()
+                if m == k:
+                    return False 
+                seen.add(m)
                 for nb in d[m]:
-                    if nb not in visited:
-                        Q.append(nb)
-        
-        return True
+                    if nb not in seen:
+                        q.append(nb)
+        return True 
