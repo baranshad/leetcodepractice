3ae6d2b8f5fed5ff920db4c80d5577b87db369e5
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-19 22:33:24 -0400

    Time: 56 ms (83.98%), Space: 14.7 MB (37.91%) - LeetHub

--- 482-license-key-formatting/482-license-key-formatting.py
+++ 482-license-key-formatting/482-license-key-formatting.py
@@ -1,8 +1,14 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        
         s = ''.join(s.split('-')).upper()[::-1]
-        res = []
-        for i in range(0, len(s), k):
-            res.append(s[i:i+k])
-        return "-".join(res)[::-1]
+        ans = []
+        for i in range(0,len(s), k):
+            ans.append(s[i:i+k])
+        res = '-'.join(ans)
+        return res[::-1]
+        
+        #s = ''.join(s.split('-')).upper()[::-1]
+        #res = []
+        #for i in range(0, len(s), k):
+            #res.append(s[i:i+k])
+        #return "-".join(res)[::-1]
