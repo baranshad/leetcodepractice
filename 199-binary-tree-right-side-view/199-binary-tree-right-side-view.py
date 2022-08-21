5f01d7d5cc0510b6bd5f7fb06c174a6cdce62755
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-21 18:28:37 -0400

    Time: 37 ms (87.35%), Space: 13.9 MB (70.69%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -5,19 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-        def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-            if not root: return []
-            res = []
-            q = deque([root]) # q.append([root]) ## q.append(root.val) is wrong
-            while q:
-                size = len(q)
-                for i in range(size):
-                    temp = q.popleft()
-                    if i == size -1:
-                        res.append(temp.val)
-                    if temp.left:
-                        q.append(temp.left)
-                    if temp.right:
-                        q.append(temp.right)    
-            return res 
-                    
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        
+        res = []
+        q = deque()
+        q.append(root)
+        while q:
+            size = len(q)
+            for i in range(size):
+                node = q.popleft()
+                if i == size-1:
+                    res.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+        return res 
+            
