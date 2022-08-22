08fadcd9c76e9e01d3da9d5a7d851585c45e6ce8
baranshad <36979586+baranshad@users.noreply.github.com>
2022-08-21 21:41:14 -0400

    Time: 147 ms (26.41%), Space: 17.9 MB (12.25%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,35 +1,17 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         rooms = []
-        for s, e in intervals:
-            rooms.append((s, True))
-            rooms.append((e, False))
-        rooms.sort()
-        num = 0
-        res = 0
-        for i, flag in rooms:
-            if flag: num += 1 
-            else: num -=1 
+        for start, end in intervals:
+            rooms.append((start, True))
+            rooms.append((end, False))
             
-            res = max(res, num)
+        rooms.sort()
+        res = count = 0
+        for i, flag in rooms:
+            if flag:
+                count +=1 
+            else:
+                count -=1 
+            res = max(res, count)
         return res 
-        
-        
-        
-        
-        #res = [x for x in set(tuple(x) for x in intervals)]
-        #s = sorted(res, key=lambda x: (x[0], x[1]))
-        #d= defaultdict(list)
-        
-        #endlist = []
-        #for i in range(len(res)):
-            #d[i] = s[i][0]
-            #endlist.append(s[i][1])
-        #for k, j in enumerate(endlist):
-            #for m in range(len(d)):
-                #if d[m] >= j:
-                    #endlist[k]=endlist[m]
-                    #d.pop(m)
-                    
-        #return len(d) + (len(intervals)-len(res))
-                    
+ 
