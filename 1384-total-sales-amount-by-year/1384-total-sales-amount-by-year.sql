2ff6ae0b32b3d9fa122fda5739d500ff64f06d16
baranshad <36979586+baranshad@users.noreply.github.com>
2022-09-06 15:22:04 -0400

    Time: 624 ms (40.32%), Space: 0B (100.00%) - LeetHub

--- 1384-total-sales-amount-by-year/1384-total-sales-amount-by-year.sql
+++ 1384-total-sales-amount-by-year/1384-total-sales-amount-by-year.sql
@@ -1,40 +1,16 @@
 # Write your MySQL query statement below
-with recursive cte as
-(select 
-start_year as report_year 
-from (select min(year(period_start)) as start_year, 
-        max(year(period_end)) as end_year 
-    from sales) a
+WITH RECURSIVE CTE AS
+    (SELECT MIN(period_start) as date
+     FROM Sales 
+     UNION ALL
+     SELECT DATE_ADD(date, INTERVAL 1 day)
+     FROM CTE
+     WHERE date <=  (SELECT MAX(period_end) FROM Sales))
 
- 
- union all 
- 
- select report_year +1 
- from cte, (select min(year(period_start)) as start_year, 
-        max(year(period_end)) as end_year 
-    from sales) b 
-    where report_year < end_year
-)
-
-
-select 
-
-Sales.product_id, 
-Product.product_name, 
-cast(cte.report_year as char) as report_year, 
-(datediff(
-case when year(period_end) != cte.report_year 
-    then cast(CONCAT(cast(cte.report_year as char), '-12','-31') as date) 
-    else period_end
-    end, 
-    
-case when year(period_start) != cte.report_year 
-    then cast(concat(cast(cte.report_year as char), '-01', '-01') as date)
-    else period_start 
-    end
-) + 1) * average_daily_sales as total_amount 
-from Sales 
-left join Product on Sales.product_id = Product.product_id 
-left join cte on cte.report_year >= year(period_start) and cte.report_year <= year(period_end)
-
-order by 1,3 asc
+SELECT 
+        s.product_id, p.product_name, LEFT(e.date,4) as report_year, SUM(s.average_daily_sales) as total_amount
+FROM Sales s
+JOIN Product p ON p.product_id = s.product_id
+JOIN CTE e ON s.period_start<=e.date AND s.period_end>=e.date
+GROUP BY 1,2,3 
+ORDER BY 1,3
