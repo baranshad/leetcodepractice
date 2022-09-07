4e8ad1c12cb82c77f07033b9a8365f04ca959321
baranshad <36979586+baranshad@users.noreply.github.com>
2022-09-06 20:32:13 -0400

    Time: 2242 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 185-department-top-three-salaries/185-department-top-three-salaries.sql
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -1,18 +1,17 @@
 # Write your MySQL query statement below
 
-
 with T1 as 
-(select A.* , B.name as dept_name
-from Employee A
+(select A.*, B.name as dep_name
+from Employee A 
 left join 
 Department B
 on A.departmentId = B.id), 
 
-T2 as
-(select * , Dense_rank () over(partition by dept_name order by salary DESC) as Den_rank 
-from T1 )
+T2 as 
+(select T1.*, dense_rank() over (partition by dep_name order by salary DESC) as "den_rank"
+from T1)
 
 
-select dept_name as Department, name as Employee, salary as Salary
-from T2
-where Den_rank <=3 
+select T2.dep_name as Department, T2.name as Employee, salary as Salary 
+from T2 
+where T2.den_rank <= 3
