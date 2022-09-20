730631e2b2487e759b1052d897e61ad6b1d32455
baranshad <36979586+baranshad@users.noreply.github.com>
2022-09-20 18:32:59 -0400

    Time: 50 ms (63.21%), Space: 13.9 MB (25.72%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,18 +1,17 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        final_int = str(int(a) + int(b)) 
-        final_str = "" 
-        carry = 0
-        for i in range(len(final_int)-1, -1, -1):
-            val = (int(final_int[i]) + carry) % 2 
-            carry = (int(final_int[i]) + carry) // 2 
-            final_str += str(val)
+        added = str(int(a)+ int(b))
+        res = ""
+        carry = 0 
+        for i in range(len(added)-1, -1, -1):
+            val = (int(added[i]) + carry) % 2 
+            carry = (int(added[i]) + carry) // 2 
+            res += str(val)
+        
         if carry:
-            final_str += "1"
-        return final_str[::-1]
-         
-      
-            
+            res = res + "1"
+        return res[::-1]
+
         #final_int, final_str, carry = str(int(a) + int(b)), "", 0
         
         #for i in range(len(final_int)-1,-1,-1):
