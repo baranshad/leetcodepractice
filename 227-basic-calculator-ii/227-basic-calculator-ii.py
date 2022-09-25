9f68e625c09bc8dbd1d64086051c8eb5783ad8e5
baranshad <36979586+baranshad@users.noreply.github.com>
2022-09-24 21:42:33 -0400

    Time: 159 ms (44.64%), Space: 16.1 MB (22.47%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -1,27 +1,25 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        s = s + "+"
-        current = 0 
-        outer = 0
-        result = 0
-        operature = "+"
-        for c in s:
-            if c == " ":
-                continue 
-            if c.isdigit():
-                current = 10* current + int(c)
-                continue 
-            if operature == "+":
-                result += outer 
-                outer = current 
-            elif operature == "-":
-                result += outer 
-                outer = -current 
-            elif operature == "*":
-                outer = outer * current 
-            elif operature == "/":
-                outer = int(outer/current)
+        num = 0
+        ope = "+"
+        stack = []
+        for i, val in enumerate(s):
+            if val.isnumeric():
+                num = num*10 + int(val)
+                print(num)
+            if val in '+-*/' or i == len(s) -1:
+                if ope == "+":
+                    stack.append(num)
+                elif ope =="-":
+                    stack.append(-num)
+                elif ope == "*":
+                    j = num*(stack.pop())
+                    stack.append(j)
+                elif ope == "/":
+                    j = int(stack.pop()/num)
+                    stack.append(j)
+                    
+                ope = val 
+                num = 0 
+        return sum(stack)
                 
-            current, operature = 0, c 
-            
-        return result + outer 
