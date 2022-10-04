7445e73ae21428035f89388ddca0eb7d72db5d93
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-03 23:21:19 -0400

    Time: 288 ms (93.82%), Space: 15.8 MB (35.64%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.py
@@ -1,29 +1,27 @@
 class Solution:
-    def pacificAtlantic(self, matrix: List[List[int]]) -> List[List[int]]:
-        if not matrix or not matrix[0]:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
             return []
         
         def dfs(i,j, visited):
             visited.add((i,j))
-            for (x,y) in [(1,0), (0,1), (-1,0), (0,-1)]:
-                nx, ny = x+i, y+j
-                if nx <0 or ny < 0 or nx >= len(matrix) or ny >= len(matrix[0]) or (nx, ny) in visited or matrix[nx][ny] < matrix[i][j]:
-                    continue 
-                visited.add((nx, ny))
-                dfs(nx, ny, visited)
-                
+            d = [(1,0),(0,1),(-1,0),(0,-1)]
+            for c in d:
+                ni, nj = i+c[0], j+c[1]
+                if ni < 0 or nj < 0 or ni >= len(heights) or nj >= len(heights[0]) or (ni,nj) in visited or heights[ni][nj] < heights[i][j]:
+                    continue
+                visited.add((ni,nj))
+                dfs(ni,nj,visited)
+            
         p = set()
         q = set()
-        for i in range(len(matrix)):
-            dfs(i, 0, p)
-            dfs(i, len(matrix[0])-1, q)
-        for i in range(len(matrix[0])):
-            dfs(0, i, p)
-            dfs(len(matrix)-1, i, q)
+        for i in range(len(heights)):
+            dfs(i,0,p)
+            dfs(i,len(heights[0])-1, q)
+            
+        for j in range(len(heights[0])):
+            dfs(0,j, p)
+            dfs(len(heights)-1, j, q)
             
         return list(p.intersection(q))
             
-            
-                
-        
-        
