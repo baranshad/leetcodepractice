64c987b6b74b3810aeb94737731e49e9e59375e8
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-04 21:51:25 -0400

    Time: 229 ms (21.26%), Space: 15.6 MB (51.86%) - LeetHub

--- 227-basic-calculator-ii/227-basic-calculator-ii.py
+++ 227-basic-calculator-ii/227-basic-calculator-ii.py
@@ -4,22 +4,31 @@ class Solution:
         num = 0 
         ope = "+"
         
-        for i,c in enumerate(s):
-            if c.isnumeric():
-                num = num*10 + int(c)
-            if c in "+-*/" or i == len(s) -1:
+        for i, val in enumerate(s):
+            if val.isnumeric():
+                num = num*10 + int(val)
+            
+            if val in "+-*/" or i == len(s)-1:
+                #print(i, val, stack, num)
+# 3+4*2
+# +3+ 4*2 
+#1 + [] 3
+#3 * [3] 4
+#4 2 [3, 4] 2
+
                 if ope == "+":
                     stack.append(num)
                 elif ope == "-":
                     stack.append(-num)
                 elif ope == "*":
                     j = stack.pop() * num 
+                    #print(j) # 8 
                     stack.append(j)
                 elif ope == "/":
-                    j = stack.pop()/num
+                    j = stack.pop()/num 
                     stack.append(int(j))
-                
+                    
                 num = 0 
-                ope = c
-
+                ope = val
+                
         return sum(stack)
