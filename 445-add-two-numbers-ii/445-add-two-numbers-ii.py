4c49ef74149809485f85f435b5e104b15801c801
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-05 20:46:20 -0400

    Time: 78 ms (87.10%), Space: 13.9 MB (88.00%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.py
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.py
@@ -16,19 +16,17 @@ class Solution:
             s2.append(l2.val)
             l2 = l2.next 
             
-        
         dummy = ListNode(0)
         carry = 0 
         while s1 or s2 or carry:
             add1 = s1.pop() if s1 else 0 
             add2 = s2.pop() if s2 else 0 
-            sum1 = add1 + add2 + carry 
-            carry = 1 if sum1 >= 10 else 0
-            sum1 = sum1 -10 if sum1 >= 10 else sum1 
+            val1 = (add1 + add2 + carry)%10
+            carry = (add1 + add2 + carry)//10
             
-            cur = ListNode(sum1)
+            cur = ListNode(val1)
             cur.next = dummy.next 
-            dummy.next = cur 
-            
-        return dummy.next
+            dummy.next =cur 
+        
+        return cur
             
