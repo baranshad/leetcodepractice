4ead2fd410c0c9312463b794edd48a35960d21a4
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-05 20:09:15 -0400

    Time: 65 ms (89.52%), Space: 13.9 MB (97.10%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -3,10 +3,9 @@ class Solution:
         s= f =0 
         n = len(chars)
         
-        while f < n:
+        while f <n:
             chars[s] = chars[f]
             count = 1 
-            
             while f < n-1 and chars[f] == chars[f+1]:
                 count += 1 
                 f += 1 
@@ -14,9 +13,9 @@ class Solution:
             if count > 1:
                 for c in str(count):
                     chars[s+1] = c
-                    s += 1
-            
-            f+= 1 
+                    s += 1 
+                    
+            f += 1 
             s += 1 
             
         return s 
