7c94ab224499f84f54f9528371ea4cb11b978fce
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-11 22:38:48 -0400

    Time: 222 ms (20.10%), Space: 16.2 MB (49.60%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -6,14 +6,13 @@ class Solution:
             points[num] += num 
             max_number = max(max_number, num) 
             
-        @cache 
-        def max_points(num):
-            if num == 0:
-                return 0 
-            if num == 1:
-                return points[1]
-            return max(max_points(num-1), max_points(num-2) + points[num])
+        max_points = [0] * (max_number + 1)
+        max_points[1] = points[1]
+
+        for num in range(2, len(max_points)):
+            # Apply recurrence relation
+            max_points[num] = max(max_points[num - 1], max_points[num - 2] + points[num])
         
-        return max_points(max_number)
+        return max_points[max_number]
             
          
