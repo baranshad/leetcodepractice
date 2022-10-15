fa5dbf9b2e7643f06476368bb7e80d2fbe085959
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-14 21:22:30 -0400

    Time: 30 ms (99.61%), Space: 14.5 MB (13.38%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -7,23 +7,25 @@
 
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        def dfs(node, par = None):
-            if node:
-                node.par = par 
-                dfs(node.left, node)
-                dfs(node.right, node)
+        def dfs(root, par=None):
+            if root:
+                root.par = par
+                dfs(root.left, root)
+                dfs(root.right, root)
                 
         dfs(root)
         
-        queue = deque([(target, 0)])
+        q = deque([(target, 0)])
         seen = {target}
-        while queue:
-            if queue[0][1] == k:
-                return [node.val for node, d in queue]
-            node, d = queue.popleft()
-            for nei in (node.left, node.right, node.par):
-                if nei and nei not in seen:
-                    seen.add(nei)
-                    queue.append((nei, d+1))
+        
+        while q:
+            if q[0][1] == k:
+                return [node.val for node, d in q]
+            
+            node, d = q.popleft()
+            for nb in (node.left, node.right, node.par):
+                if nb and nb not in seen:
+                    seen.add(nb)
+                    q.append((nb, d+1))
                     
-        return [] 
+        return []
