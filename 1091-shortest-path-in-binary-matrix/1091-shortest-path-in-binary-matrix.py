567f721b362932a695822041d9efeb01b2d8778d
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-16 21:18:40 -0400

    Time: 584 ms (96.35%), Space: 14.4 MB (65.66%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,47 +1,32 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        max_row = len(grid) - 1
-        max_col = len(grid[0]) - 1
-        directions = [
-            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+        m = len(grid)
+        n = len(grid[0])
+        d = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
         
-        # Helper function to find the neighbors of a given cell.
-        def get_neighbours(row, col):
-            for row_difference, col_difference in directions:
-                new_row = row + row_difference
-                new_col = col + col_difference
-                if not(0 <= new_row <= max_row and 0 <= new_col <= max_col):
-                    continue
-                if grid[new_row][new_col] != 0:
-                    continue
-                yield (new_row, new_col)
+        def nbs(r,c):
+            for dx, dy in d:
+                nx, ny = r+dx, c+dy 
+                if nx < 0 or ny < 0 or nx>= m or ny >= n or grid[nx][ny] != 0:
+                    continue 
+                yield(nx, ny)
                 
-        if grid[0][0] != 0 or grid[max_row][max_col] != 0:
-            return -1
+        if grid[0][0] != 0 or grid[m-1][n-1] !=0:
+            return -1 
         
-        queue = deque()
-        queue.append((0, 0))
+        q =deque()
+        q.append((0,0))
         grid[0][0] = 1 
-        
-        
-        # Carry out the BFS.
-        while queue:
-            row, col = queue.popleft()
-            distance = grid[row][col]
-            if (row, col) == (max_row, max_col):
-                return distance
-            for neighbour_row, neighbour_col in get_neighbours(row, col):
-                grid[neighbour_row][neighbour_col] = distance + 1
-                queue.append((neighbour_row, neighbour_col))
-        
-        # There was no path.
-        return -1
-        
-        
-        
-        
-        
-        
+        while q:
+            r, c = q.popleft()
+            distance = grid[r][c]
+            if (r,c) == (m-1, n-1):
+                return distance 
+            for nx, ny in nbs(r,c):
+                grid[nx][ny] = distance + 1 
+                q.append((nx,ny))
+                
+        return -1 
         
         
         
