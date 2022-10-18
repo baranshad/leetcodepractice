8b551378340411906b2d293ea1efe94f9f9dc2fc
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-17 21:38:52 -0400

    Time: 60 ms (43.35%), Space: 13.8 MB (98.94%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,11 +1,26 @@
 class Solution:
     def generateMatrix(self, n: int) -> List[List[int]]:
-        A = [[0] * n for _ in range(n)]
-        i, j, di, dj = 0, 0, 0, 1
-        for k in range(n*n):
-            A[i][j] = k + 1
-            if A[(i+di)%n][(j+dj)%n]:
-                di, dj = dj, -di
-            i += di
-            j += dj
-        return A
+        stack = [(0,1),(1,0),(0,-1),(-1,0)]
+        m = [[0 for _ in range(n)] for x in range(n)]
+        num = 2
+        total = n*n
+        i=j=0
+        side_step = 1
+        side_len = n
+        turn = 1
+        m[0][0]=1
+        while num <= total:
+            if side_step == side_len:
+                side_step=0
+                stack.append(stack.pop(0))
+                turn+=1
+                if turn == 2:
+                    side_len-=1
+                    turn = 0
+            i=i+stack[0][0]
+            j=j+stack[0][1]
+            m[i][j] = num
+            num+=1
+            side_step +=1
+            
+        return m
