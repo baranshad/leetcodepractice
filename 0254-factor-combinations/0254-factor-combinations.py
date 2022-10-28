5e33f12598174c18c072a37ac281094d1ea7bc84
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-27 22:11:07 -0400

    Time: 93 ms (94.14%), Space: 15.1 MB (16.25%) - LeetHub

--- 0254-factor-combinations/0254-factor-combinations.py
+++ 0254-factor-combinations/0254-factor-combinations.py
@@ -1,27 +1,16 @@
 class Solution:
     def getFactors(self, n: int) -> List[List[int]]:
         res = []              
-        factors = []           
-        def dfs(start, n):
-            if factors:      
-                res.append(factors + [n])
+        #factors = []           
+        def dfs(temp, start, n):
+            if temp:      
+                res.append(temp[:] + [n])
             for i in range(start, int(sqrt(n))+1):  
                 if n%i == 0:
-                    factors.append(i)
-                    dfs(i, n//i)
-                    factors.pop()
-        dfs(2, n)
+                    temp.append(i)
+                    dfs(temp, i, n//i)
+                    temp.pop()
+        dfs([], 2, n)
         return res
                 
-        #res = []              
-        #factors = []           
-        #def dfs(start, n):
-            #if factors:       # ensure x != n
-               # res.append(factors + [n])
-            #for i in range(start, int(sqrt(n))+1):  n
-                #if n%i == 0:
-                   # factors.append(i)
-                   # dfs(i, n/i)
-                   # factors.pop()
-        #dfs(2, n)
-        #return res
+ 
