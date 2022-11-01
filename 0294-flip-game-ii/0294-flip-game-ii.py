209b7f712b88b136e8efacaefafddf8b9f9a159a
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-31 21:26:28 -0400

    Time: 96 ms (79.44%), Space: 15.2 MB (43.55%) - LeetHub

--- 0294-flip-game-ii/0294-flip-game-ii.py
+++ 0294-flip-game-ii/0294-flip-game-ii.py
@@ -3,14 +3,14 @@ class Solution:
         def helper(s):
             if s in memo:
                 return memo[s]
-        
+            
             for i in range(len(s)-1):
-                if s[i]=='+' and s[i+1]=='+' and not helper(s[:i]+'--'+s[i+2:]):
-                    memo[s]=True
-                    return True
-            memo[s]=False
-            return False
-    
-    
-        memo={}
+                if s[i] == "+" and s[i+1] == "+" and not helper(s[:i] + "--" + s[i+2:]):
+                    memo[s] = True 
+                    return True 
+                
+            memo[s] = False 
+            return False 
+        
+        memo = {}
         return helper(currentState)
