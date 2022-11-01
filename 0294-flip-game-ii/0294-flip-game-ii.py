fe839403114c1e1106559b1ca6198d52246bc134
baranshad <36979586+baranshad@users.noreply.github.com>
2022-10-31 20:10:15 -0400

    Time: 120 ms (66.53%), Space: 15.3 MB (19.76%) - LeetHub

---
+++ 0294-flip-game-ii/0294-flip-game-ii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canWin(self, currentState: str) -> bool:
+        def helper(s):
+            if s in memo:
+                return memo[s]
+        
+            for i in range(len(s)-1):
+                if s[i]=='+' and s[i+1]=='+' and not helper(s[:i]+'--'+s[i+2:]):
+                    memo[s]=True
+                    return True
+            memo[s]=False
+            return False
+    
+    
+        memo={}
+        return helper(currentState)
