15c652ddf3dd962adf0a151b38096b0dec60a0e4
baranshad <36979586+baranshad@users.noreply.github.com>
2022-11-01 23:25:33 -0400

    Time: 42 ms (93.86%), Space: 13.8 MB (98.98%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        h = defaultdict(int)
+        bulls = cows = 0 
+        
+        for i, s in enumerate(secret):
+            g = guess[i]
+            if s == g:
+                bulls += 1 
+                
+            else:
+                cows += int(h[s] < 0) + int(h[g] > 0)
+                h[s] += 1 
+                h[g] -= 1 
+                
+        return "{}A{}B".format(bulls, cows)
