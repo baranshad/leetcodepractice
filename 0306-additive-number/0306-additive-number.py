4829492410fd02ed2e12db89e250f00ea25f1641
baranshad <36979586+baranshad@users.noreply.github.com>
2022-11-02 20:37:26 -0400

    Time: 37 ms (89.04%), Space: 13.8 MB (81.47%) - LeetHub

--- 0306-additive-number/0306-additive-number.py
+++ 0306-additive-number/0306-additive-number.py
@@ -1,31 +1,16 @@
 class Solution:
     def isAdditiveNumber(self, num: str) -> bool:
         n = len(num)
-        if n < 3:
-            return False
-
-        for second in range(1, 1 + (n - 1) // 2):       # first digit of second integer
-            if num[0] == "0" and second > 1:
-                break
-            third = second + 1                          # first digit of third integer
-
-            while n - third >= max(second, third - second):
-                if num[second] == "0" and third > second + 1:
+        for i, j in itertools.combinations(range(1, n), 2):
+            a, b = num[:i], num[i:j]
+            if a != str(int(a)) or b != str(int(b)):
+                continue
+            while j < n:
+                c = str(int(a) + int(b))
+                if not num.startswith(c, j):
                     break
-
-                n1, n2 = int(num[0:second]), int(num[second:third])
-                start = third
-                while True:
-                    next_int = n1 + n2
-                    next_start = start + len(str(next_int))
-                    if num[start] == "0" and next_start > start + 1:
-                        break
-                    if next_int != int(num[start:next_start]):
-                        break
-                    if next_start == n:
-                        return True
-                    n1, n2, start = n2, next_int, next_start
-
-                third += 1
-
+                j += len(c)
+                a, b = b, c
+            if j == n:
+                return True
         return False
